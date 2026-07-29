e654d737f0f3220312fed3bda53276f09579981f
Tanvir Hossan Ramim <132383235+Tanvir-Ramim@users.noreply.github.com>
2026-07-29 17:53:01 +0600

    project file setup done

---
+++ app/(dashboardGroup)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashBoardGroupLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default DashBoardGroupLayout;

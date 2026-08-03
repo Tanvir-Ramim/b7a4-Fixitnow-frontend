f09b388e7c00ca6d83b0bc1c835f7356622d23bf
Tanvir Hossan Ramim <132383235+Tanvir-Ramim@users.noreply.github.com>
2026-08-04 02:37:31 +0600

    error fixed

--- app/(publicGroup)/page.tsx
+++ app/(publicGroup)/page.tsx
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import HomePageBanner from "./_components/HomePageBanner";
 import HomePageCategory from "./_components/HomePageCategory";
 import StartPosting from "./_components/StartPosting";
 import LatestServices from "./_components/LatestServices";
-
 
 const page = () => {
   return (
     <div>
       <HomePageBanner></HomePageBanner>
       <div className="max-w-375 lg:px-9 md:px-6 px-3  md:space-y-18 space-y-12  mx-auto">
-        <HomePageCategory></HomePageCategory>
+        <Suspense fallback={<div>.</div>}>
+          <HomePageCategory></HomePageCategory>
+        </Suspense>
+
         <StartPosting></StartPosting>
       </div>
       <LatestServices></LatestServices>
-  
     </div>
   );
 };
